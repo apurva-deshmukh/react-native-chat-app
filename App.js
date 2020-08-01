dc55979fadd49722a69a6859d668dce7387e0799
apurva-deshmukh <aadeshmukh13@gmail.com>
2020-08-01 15:27:59 -0400

    connect to backend

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import io from "socket.io-client";
 
 export default function App() {
+  useEffect(function () {
+    io("http://169.254.126.228:3001");
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -14,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
